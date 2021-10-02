71cbbeed6cd8d17ba1f4161079bb1392331cf1f7
Fernando <63949290@qq.com>
2021-10-03 07:25:44 +0800

    add appfilter

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,6 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/ziyifeihu/openwrtAPPfilter' >>feeds.conf.default
 
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
